c712a79f95ae34218c45db3a29186766db39f24a
Vasilii Piiadov <vasily.piyadov@icloud.com>
2023-09-21 21:41:58 -0300

    mutate, remove any capybara from any thread

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,72 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use std::collections::HashMap;
+
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
 use crossbeam;
 use indexmap::IndexMap;
+use indexmap::map::MutableKeys;
 use nohash_hasher::BuildNoHashHasher;
 use rand::Rng;
 use std::thread;
 
-#[derive(Debug)]
-struct Capybara {
-    a: usize,
-    b: usize,
-}
-
-impl Capybara {
-    pub fn new(a: usize) -> Option<Capybara> {
-        Some(Capybara { a, b: a })
-        //None
-    }
-}
-
-#[derive(Debug)]
-struct Cell {
-    p: usize,
-    capybara: Option<Capybara>,
-}
-
-impl Cell {
-    pub fn new(p: usize) -> Cell {
-        Cell {
-            p,
-            capybara: Capybara::new(p),
-        }
-    }
-}
-
-struct CellsMap {
-    map: IndexMap<usize, Cell, BuildNoHashHasher<usize>>,
-}
-
-impl CellsMap {
-    pub fn new(size: usize) -> CellsMap {
-        CellsMap {
-            map: IndexMap::with_capacity_and_hasher(size, BuildNoHashHasher::default()),
-        }
-    }
-}
-
-struct World {
-    map: IndexMap<usize, Arc<Mutex<Cell>>, BuildNoHashHasher<usize>>,
-}
-
-impl World {
-    pub fn new(size: usize) -> World {
-        World {
-            map: IndexMap::with_capacity_and_hasher(size, BuildNoHashHasher::default()),
-        }
-    }
-}
-
 fn test2(size: usize, tests_num: usize) {
-    println!("\n===== TEST 2 =====");
-    println!("----- Write CellsMap performance-----");
-    for _ in 0..tests_num {
-        let mut cmap = CellsMap::new(size);
-        let t0 = Instant::now();
-        for i in 0_usize..size {
-            cmap.map.insert(i, Cell::new(i));
-        }
-        let elapsed = t0.elapsed().as_secs_f64();
-        println!("Time elapsed: {:.3} sec", elapsed);
-    }
-
     {
+        println!("\n===== TEST 2 =====");
+        println!("----- Write CellsMap performance-----");
+
+        #[derive(Debug)]
+        struct Capybara {
+            a: usize,
+            b: usize,
+        }
+
+        impl Capybara {
+            pub fn new(a: usize) -> Option<Capybara> {
+                Some(Capybara { a, b: a })
+                //None
+            }
+        }
+
+        #[derive(Debug)]
+        struct Cell {
+            p: usize,
+            capybara: Option<Capybara>,
+        }
+
+        impl Cell {
+            pub fn new(p: usize) -> Cell {
+                Cell {
+                    p,
+                    capybara: Capybara::new(p),
+                }
+            }
+        }
+
+        struct CellsMap {
+            map: IndexMap<usize, Cell, BuildNoHashHasher<usize>>,
+        }
+
+        impl CellsMap {
+            pub fn new(size: usize) -> CellsMap {
+                CellsMap {
+                    map: IndexMap::with_capacity_and_hasher(size, BuildNoHashHasher::default()),
+                }
+            }
+        }
+
+        for _ in 0..tests_num {
+            let mut cmap = CellsMap::new(size);
+            let t0 = Instant::now();
+            for i in 0_usize..size {
+                cmap.map.insert(i, Cell::new(i));
+            }
+            let elapsed = t0.elapsed().as_secs_f64();
+            println!("Time elapsed: {:.3} sec", elapsed);
+        }
+
         println!("----- Lookup performance -----");
 
         let mut cmap = CellsMap::new(size);
@@ -130,6 +120,44 @@ fn test2(size: usize, tests_num: usize) {
     {
         // Mutable slices
         println!("\nMutable slices, keep owner:\n");
+        #[derive(Debug)]
+        struct Cell {
+            p: usize,
+            capybara: Option<Capybara>,
+        }
+
+        impl Cell {
+            pub fn new(p: usize) -> Cell {
+                Cell {
+                    p,
+                    capybara: Capybara::new(p),
+                }
+            }
+        }
+
+        struct CellsMap {
+            map: IndexMap<usize, Cell, BuildNoHashHasher<usize>>,
+        }
+
+        impl CellsMap {
+            pub fn new(size: usize) -> CellsMap {
+                CellsMap {
+                    map: IndexMap::with_capacity_and_hasher(size, BuildNoHashHasher::default()),
+                }
+            }
+        }
+        #[derive(Debug)]
+        struct Capybara {
+            a: usize,
+            b: usize,
+        }
+
+        impl Capybara {
+            pub fn new(a: usize) -> Option<Capybara> {
+                Some(Capybara { a, b: a })
+                //None
+            }
+        }
 
         let mut cmap = CellsMap::new(size);
         for i in 0_usize..size {
@@ -158,10 +186,48 @@ fn test2(size: usize, tests_num: usize) {
 
         println!("CellsMap: {:?}", cmap.map.get_index(10)); // Finally, CellsMap data is not borrowed
     }
-    
+
     {
         // Mutable cells in threads
         println!("\nThreads:\n");
+        #[derive(Debug)]
+        struct Cell {
+            p: usize,
+            capybara: Option<Capybara>,
+        }
+
+        impl Cell {
+            pub fn new(p: usize) -> Cell {
+                Cell {
+                    p,
+                    capybara: Capybara::new(p),
+                }
+            }
+        }
+
+        struct CellsMap {
+            map: IndexMap<usize, Cell, BuildNoHashHasher<usize>>,
+        }
+
+        impl CellsMap {
+            pub fn new(size: usize) -> CellsMap {
+                CellsMap {
+                    map: IndexMap::with_capacity_and_hasher(size, BuildNoHashHasher::default()),
+                }
+            }
+        }
+        #[derive(Debug)]
+        struct Capybara {
+            a: usize,
+            b: usize,
+        }
+
+        impl Capybara {
+            pub fn new(a: usize) -> Option<Capybara> {
+                Some(Capybara { a, b: a })
+                //None
+            }
+        }
 
         let mut cmap = CellsMap::new(size);
         for i in 0_usize..size {
@@ -200,85 +266,130 @@ fn test2(size: usize, tests_num: usize) {
 
     println!("\nThreads access to random cell:\n");
 
-    let mut world = World::new(size);
-    for i in 0_usize..size {
-        world.map.insert(i, Arc::new(Mutex::new(Cell::new(i))));
+    #[derive(Debug)]
+    struct Capybara {
+        a: usize,
+        b: usize,
+        to_remove: bool,
     }
 
+    impl Capybara {
+        pub fn new(param: usize) -> Capybara {
+            Capybara { a: param, b: param, to_remove: false }
+        }
+    }
+
+    // #[derive(Debug)]
+    // struct Area {
+    //     p: usize,
+    // }
+
+    // impl Area {
+    //     pub fn new(p: usize) -> Area {
+    //         Area { p }
+    //     }
+    // }
+
+    // struct World {
+    //     layout: IndexMap<usize, Mutex<Area>, BuildNoHashHasher<usize>>,
+    // }
+
+    // impl World {
+    //     pub fn new(size: usize) -> Arc<World> {
+    //         Arc::new(World {
+    //             layout: IndexMap::with_capacity_and_hasher(size, BuildNoHashHasher::default()),
+    //         })
+    //     }
+    // }
+
+    type Population = IndexMap<usize, Mutex<Capybara>, BuildNoHashHasher<usize>>;
+
+    let mut population: Population = IndexMap::with_hasher(BuildNoHashHasher::default());
+    for i in 0..10usize {
+        population.insert(i, Mutex::new(Capybara::new(i)));
+    }
+
+    let keys = population.keys().collect::<Vec<&usize>>();
     
-    let selected_cells: Vec<&mut Arc<Mutex<Cell>>> = world
-    .map
-    .values_mut()
-    .filter(|cell| { 
-        let m = cell.lock().unwrap();
-        m.p >= 10 && m.p < 15
-    })
-    .collect();
 
-
+    let t0 = Instant::now();
     crossbeam::scope(|s| {
-        for chunk in selected_cells.chunks(2){
-            
+        let population = &population;
+        
+        for keys_chunk in keys.chunks(3) {
             s.spawn(move |_| {
-
+                let thread_id = format!("{:?}", thread::current().id());
+                let num = rand::thread_rng().gen_range(0..500);
+                thread::sleep(Duration::from_millis(num));
                 
-                {   // mutate "own" cell from chunk
-                    let mut m = chunk[0].lock().unwrap();
-                    m.p = 222;
+                let k = *keys_chunk.first().unwrap();
+                
+                {   // mutate any capybara (first from the key_chunk)
+                    let mut m = population.get(k).unwrap().lock().unwrap();
+                    m.a = rand::thread_rng().gen_range(0..500);
                 }
-                println!("Chunk {:?}", chunk);
-
-                {   // mutate any cell from the world
-
-                    //let a = world.map.first().unwrap().1;
-
-
+                println!("{:?}, sleep: {}, key: {}, {:?}", thread_id, num, k, population.get(k).unwrap());
+                
+                {   // mark any capybara (first from the key_chunk) to remove
+                    let mut m = population.get(k).unwrap().lock().unwrap();
+                    m.to_remove = true;
                 }
+                
             });
         }
-    
-        
-
     })
     .unwrap();
+    let elapsed = t0.elapsed().as_secs_f64();
+    println!("Time elapsed: {:.3} sec", elapsed);
 
-    println!("\nSelected cells: {:?}", selected_cells[0]);
-    println!("World: {:?}", world.map.get_index(10));
-    
+    // remove capybaras if to_remove == true   retain, keys, par_keys
+    population.retain(|_, m| !m.get_mut().unwrap().to_remove);
 
-}
 
-fn test1(size: usize, tests_num: usize) {
-    println!("\n===== TEST 1 =====");
-    //let mut hm: HashMap::<usize, Cell> = HashMap::new();
-    //let mut hm: HashMap::<usize, Cell, BuildNoHashHasher<usize>> = HashMap::with_capacity_and_hasher(size, BuildNoHashHasher::default());
-    let mut hm: HashMap<usize, Cell, BuildNoHashHasher<usize>> =
-        HashMap::with_hasher(BuildNoHashHasher::default());
-    {
-        println!("----- Write HashMap/BuildNoHashHasher (nohash-hasher 0.2.0) -----");
-        for _ in 0..tests_num {
-            let t0 = Instant::now();
-            for i in 0_usize..size {
-                hm.insert(i, Cell::new(i));
-            }
-            let elapsed = t0.elapsed().as_secs_f64();
-            println!("Time elapsed: {:.3} sec", elapsed);
-        }
-    }
-    {
-        println!("----- Read HashMap/BuildNoHashHasher (nohash-hasher 0.2.0) -----");
-        for _ in 0..tests_num {
-            let t0 = Instant::now();
-            let mut sum: f32 = 0f32;
-            for i in 0_usize..size {
-                if let Some(x) = hm.get(&i) {
-                    sum += x.p as f32;
-                }
-            }
-            let elapsed = t0.elapsed().as_secs_f64();
-            println!("The sum is: {}. Time elapsed: {:.3} sec", sum, elapsed);
-        }
-    }
+    // let mut world = World::new(size);
+    // for i in 0_usize..size {
+    //     world.layout.insert(i, Mutex::new(Area::new(i)));
+    // }
+
+    println!(
+        "\nPopulation of {}, first: {:?}",
+        population.len(),
+        population[0]
+    );
+
+    // let selected_cells: Vec<&mut Mutex<Area>> = world
+    // .layout
+    // .values_mut()
+    // .filter(|cell| {
+    //     let m = cell.lock().unwrap();
+    //     m.p >= 10 && m.p < 15
+    // })
+    // .collect();
+
+    // crossbeam::scope(|s| {
+    //     for chunk in selected_cells.chunks(2){
+
+    //         s.spawn(move |_| {
+
+    //             {   // mutate "own" cell from chunk
+    //                 let mut m = chunk[0].lock().unwrap();
+    //                 m.p = 222;
+    //             }
+    //             println!("Chunk {:?}", chunk);
+
+    //             {   // mutate any cell from the world
+
+    //                 //let a = world.map.first().unwrap().1;
+
+    //             }
+    //         });
+    //     }
+
+    // })
+    // .unwrap();
+
+    // println!("\nSelected cells: {:?}", selected_cells[0]);
+    // println!("World: {:?}", world.layout.get_index(10));
 }
 
 fn main() {
@@ -287,6 +398,5 @@ fn main() {
 
     println!("HashMap size: {}", size);
 
-    test1(size, tests_num);
     test2(size, tests_num);
 }
